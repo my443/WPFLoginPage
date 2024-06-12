3b18dbc2f3de3a48a0d225b44e04b0dfb139aeeb
John van Dijk <fortyfour-three@teksavvy.com>
2024-06-12 19:37:20 -0400

    Add project files.

---
+++ LoginWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string usernameText = UserName.Text;
+            string passwordText = PasswordBox.Password;
+            LoginBorder.Visibility = Visibility.Collapsed;
+            textBoxMultiLine.Visibility = Visibility.Visible;
+            headingTextBlock.Visibility = Visibility.Visible;
+
+            AddRowToTextBox("Username: " + usernameText);
+            AddRowToTextBox("Password: " + passwordText);
+        }
+
+        private void AddRowToTextBox(string text)
+        {
+            textBoxMultiLine.Text += "\n"+text;
+        }
+    }
+}
